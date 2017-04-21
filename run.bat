1a8171a42911054d0ba2c2f24a3d488b462b2b75
Toshi Piazza <toshi.piazza@gmail.com>
2017-04-21 12:43:52 -0400

    Attempt 2, change png file output

--- run.bat
+++ run.bat
@@ -1,4 +1,5 @@
 rem build\bin\Debug\optixHello.exe -f output\out.ppm
 rem build\bin\Debug\optixSphere.exe -f output\out.ppm
 del output\out.ppm
+del output\out.png
 build\bin\Debug\optixWhitted.exe -f output\out.ppm -n
