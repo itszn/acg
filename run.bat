6c5a5b9c9b088682ddeaf24b4c3ff6748909586a
Toshi Piazza <toshi.piazza@gmail.com>
2017-04-21 12:39:46 -0400

    Is output file being correctly written?

--- run.bat
+++ run.bat
@@ -1,3 +1,4 @@
 rem build\bin\Debug\optixHello.exe -f output\out.ppm
 rem build\bin\Debug\optixSphere.exe -f output\out.ppm
+del output\out.ppm
 build\bin\Debug\optixWhitted.exe -f output\out.ppm -n
