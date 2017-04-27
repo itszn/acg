3649f0da59e559e7c1db3ff26cd7af0fc28bcf81
Toshi Piazza <toshi.piazza@gmail.com>
2017-04-26 23:29:07 -0400

    bunny bunny

--- run.bat
+++ run.bat
@@ -2,4 +2,4 @@ rem build\bin\Debug\optixHello.exe -f output\out.ppm
 rem build\bin\Debug\optixSphere.exe -f output\out.ppm
 del output\out.ppm
 del output\out.png
-build\bin\Debug\optixWhitted.exe src\optixWhitted\bunny_1k.obj output\out.ppm
+build\bin\Debug\optixWhitted.exe src\optixWhitted\bunny_40k.obj output\out.ppm
