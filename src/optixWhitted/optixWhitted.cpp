bafb940f37c16966520fea94e9cddb80daf57d22
Toshi Piazza <toshi.piazza@gmail.com>
2017-04-26 22:16:14 -0400

    move barb's object

--- src/optixWhitted/optixWhitted.cpp
+++ src/optixWhitted/optixWhitted.cpp
@@ -101,6 +101,12 @@ auto create_triangle(Context &context,
     trans.setRow(2, make_float4( 0.0, 0.0,10.0, 0.0));
     trans.setRow(3, make_float4( 0.0, 0.0, 0.0, 1.0));
 
+    Matrix4x4 rotn;
+    trans.setRow(0, make_float4( 1.0, 0.0, 0.0, 0.0));
+    trans.setRow(1, make_float4( 0.0, 0.0,-1.0, 0.0));
+    trans.setRow(2, make_float4( 0.0, 1.0, 0.0, 0.0));
+    trans.setRow(3, make_float4( 0.0, 0.0, 0.0, 1.0));
+
     // TODO: Optimize. Can we have a mesh geometry with many primitives?
     // This will allow us to use many more rays for shadow computation
     std::string triangle_ptx = ptxPath("triangle.cu");
@@ -110,9 +116,9 @@ auto create_triangle(Context &context,
             context->createProgramFromPTXFile(triangle_ptx, "bounds"));
     triangle->setIntersectionProgram(
             context->createProgramFromPTXFile(triangle_ptx, "robust_intersect"));
-    triangle["x"]->setFloat(make_float3(make_float4(x)*trans) + make_float3(0.0,-0.5,-2.0));
-    triangle["y"]->setFloat(make_float3(make_float4(y)*trans) + make_float3(0.0,-0.5,-2.0));
-    triangle["z"]->setFloat(make_float3(make_float4(z)*trans) + make_float3(0.0,-0.5,-2.0));
+    triangle["x"]->setFloat(make_float3(make_float4(x)*trans*rotn) + make_float3(0.0,-0.5,-2.0));
+    triangle["y"]->setFloat(make_float3(make_float4(y)*trans*rotn) + make_float3(0.0,-0.5,-2.0));
+    triangle["z"]->setFloat(make_float3(make_float4(z)*trans*rotn) + make_float3(0.0,-0.5,-2.0));
 
     // metal material
     const std::string metal_ptx = ptxPath( "toon.cu" );
